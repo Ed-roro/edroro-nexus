9ab89159a33f633169df004a96fbac0f2abce890
Ed-Matic <eigharoro@gmail.com>
2025-03-10 16:11:00 -0400

    feat(styles): add styling to the src

---
+++ src/styles/index.js
@@ -0,0 +1,13 @@
+import { Inter, Poppins, Roboto, Overpass_Mono } from "next/font/google";
+
+export const roboto = Roboto({ subsets: ["latin"], weight: "400" });
+
+export const poppins300 = Poppins({ subsets: ["latin"], weight: "300" });
+export const poppins400 = Poppins({ subsets: ["latin"], weight: "400" });
+export const poppins500 = Poppins({ subsets: ["latin"], weight: "500" });
+export const poppins600 = Poppins({ subsets: ["latin"], weight: "600" });
+export const poppins700 = Poppins({ subsets: ["latin"], weight: "700" });
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const mono = Overpass_Mono({ subsets: ["latin"] });
